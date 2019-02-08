3e0dcbdbecc6d90240599663c5f967c21d8db8b9
georgfalemark <georg@falemark.se>
2019-02-08 13:17:43 +0100

    shopping cart

--- Mvc02/Controllers/ShoppingCartController.cs
+++ Mvc02/Controllers/ShoppingCartController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Mvc02.Data;
+using Mvc02.Models.Entities;
 using Mvc02.Models.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,6 +14,12 @@ namespace Mvc02.Controllers
     public class ShoppingCartController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly ShoppingCart _shoppingCartContext;
+
+        public ShoppingCartController(ShoppingCart shoppingCart)
+        {
+            _shoppingCartContext = shoppingCart;
+        }   
 
         public ShoppingCartController(ApplicationDbContext context)
         {
@@ -21,13 +28,13 @@ namespace Mvc02.Controllers
 
         public ViewResult Index()
         {
-            var items = _context.ShoppingCarts.GetShoppingCartItems();
-            _context.ShoppingCartItems = items;
+            var items = _shoppingCartContext.GetShoppingCartItems();
+            _shoppingCartContext.ShoppingCartItems = items;
 
             var shoppingCartViewModel = new ShoppingCartVm
             {
-                ShoppingCart = _context.ShoppingCarts,
-                ShoppingCartTotal = _context.ShoppingCarts.GetShoppingCartTotal()
+                ShoppingCart = _shoppingCartContext,
+                ShoppingCartTotal = _shoppingCartContext.GetShoppingCartTotal()
             };
 
             return View(shoppingCartViewModel);
@@ -39,7 +46,7 @@ namespace Mvc02.Controllers
 
             if (selectedProduct != null)
             {
-                _context.ShoppingCarts.AddToCart(selectedProduct, 1);
+                _shoppingCartContext.AddToCart(selectedProduct, 1);
             }
             return RedirectToAction("Index");
         }
@@ -50,7 +57,7 @@ namespace Mvc02.Controllers
 
             if (selectedProduct != null)
             {
-                _context.ShoppingCarts.RemoveFromCart(selectedProduct);
+                _shoppingCartContext.RemoveFromCart(selectedProduct);
             }
             return RedirectToAction("Index");
         }
